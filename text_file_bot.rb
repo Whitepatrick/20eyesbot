e17176b366ed396c08156afb0ca463d24ee10ae5
Patrick White <whitep@vcu.edu>
2015-09-11 13:50:13 -0400

    moving some things around, refactored text update bot, almost ready to merge to master

--- text_file_bot.rb
+++ text_file_bot.rb
@@ -2,16 +2,25 @@ require 'anemone'
 require 'twitter'
 require 'open-uri'
 require_relative 'credentials'
+require_relative 'hashtags'
+
+=begin
+Text File Bot LIVES!
+This bot does:
+- takes a predefined url stored in a global variable (SCRAPE_URL)
+- returns an array of all links (files) on page
+- grabs a random text file url from the url array
+- begins a tweet cycle that tweets every line and sleeps for a b
+=end
 
 SCRAPE_URL = "http://www.textfiles.com/humor/COMPUTER/"
-HASHTAGS = ['#followback', '#followforfollow', '#ftw',
-            '#taylorswift', '#trump2016','#tfw',
-            '#guyfieri', "#bieber", '#onedirection',
-            '#followme']
+
 
 class TextFileBot
+
   def initialize
     @url_array = Array.new
+    @url
   end
 
   def rest_connector
@@ -23,24 +32,29 @@ class TextFileBot
     end
   end
 
-  def crawl_for_urls
+  def get_url_array
+    arr = Array.new
     Anemone.crawl(SCRAPE_URL, :discard_page_bodies => true) do |anemone|
-      anemone.on_every_page { |page| @url_array.push(page.url.to_s)}
+      anemone.on_every_page { |page| arr.push(page.url.to_s)}
     end
-    @url_array.shift(5)
+    return arr
   end
 
-  def update_with_txt_file(selected_url, sleep_time)
-    txt_file = open(selected_url.sample)
-    txt_file.each do |line|
-      #rest_connector.update("#{line.strip} #{hashtags}")
-      p "Now tweeting #{line.to_s.strip} #{HASHTAGS.sample} #{HASHTAGS.sample}"
+  def get_url_from_url_array
+    @url_array = self.get_url_array
+    @url_array.delete_if { |i| i == "http://www.textfiles.com/humor/COMPUTER/BOFH/" }
+    @url = @url_array.sample
+  end
+
+  def update_with_txt_file(sleep_time)
+    file_url = open(get_url_from_url_array)
+    file_url.each do |file_line|
+      rest_connector.update("#{file_line.to_s.strip} #{HASHTAGS.sample} #{HASHTAGS.sample}  #{HASHTAGS.sample}")
+      p "#{file_line.to_s.strip} #{HASHTAGS.sample} #{HASHTAGS.sample}  #{HASHTAGS.sample}"
       sleep(sleep_time)
     end
   end
-end
 
+end
 tfb = TextFileBot.new
-#tfb.crawl_for_urls
-file_to_scrape = tfb.crawl_for_urls
-tfb.update_with_txt_file(file_to_scrape)
+tfb.update_with_txt_file(0.5)
