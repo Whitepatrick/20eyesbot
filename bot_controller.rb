3ee4dc6fbd9901aabec2f7913cdfd8654dd5443c
Patrick White <whitep@vcu.edu>
2015-09-12 19:43:22 -0400

    adding findr bot to follow a metric shit ton of people

--- bot_controller.rb
+++ bot_controller.rb
@@ -8,21 +8,24 @@ require_relative 'txt_updatr'
 require_relative 'followr'
 require_relative 'searchr'
 require_relative 'streamr'
+require_relative 'findr'
 
 swarm = []
 fbb = FollowBackBot.new
 tfb = TextFileBot.new
 sadb = SearchAndDestroyBot.new
 ssb = StreamSiteBot.new
+fafb = FindAndFollowBot.new
 
-puts "Started at: #{Time.now}"
 followr = Thread.new { fbb.follow_back }
 txt_updatr = Thread.new { tfb.update_with_txt_file(3600) }
 searchr = Thread.new { sadb.twitter_search }
 streamr = Thread.new { ssb.stream_site }
+findr = Thread.new { fafb.find_and_follow }
 swarm.push(followr)
 swarm.push(txt_updatr)
 swarm.push(searchr)
+swarm.push(findr)
 
 p "Started at: #{Time.now}"
 swarm.each { |t| t.join }
--- findr.rb
+++ findr.rb
@@ -5,14 +5,15 @@ require_relative 'bot_connector'
 class FindAndFollowBot < BotConnector
 
   def find_and_follow
-    HASHTAGS.each do |tag|
-      rest_connector.search(tag, result_type: "recent").take(3).each do |tweet|
+    HASHTAGS.reverse.each do |tag|
+      puts "following users using the tag: #{tag}"
+      rest_connector.search(tag, result_type: "recent").take(5).each do |tweet|
         rest_connector.follow(tweet.user.id)
-        p "Now following: #{tweet.user.id}"
-        puts "\n"
-        sleep(90)
+        p "Now following: #{tweet.user.screen_name}"
       end
+      sleep(301)
     end
+    rest_connector.update("Hey @operations_ivy, I've finished your unholy biding.")
   end
 
 end
